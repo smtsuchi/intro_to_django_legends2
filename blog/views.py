2fbccddc245abcc010c851fcaec52cbdbd667123
smtsuchi <shoha1999@gmail.com>
2022-02-21 17:15:59 -0500

    day1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hey there! this is our blog home page!")
+
+def posts(request):
+    return HttpResponse("This is the posts page")
+
+def createPost(request):
+    return HttpResponse("This is the create post page")
